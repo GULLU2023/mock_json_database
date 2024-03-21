3a2a07dcdb8cd116db6ad6fa8bf0c7ea6701c2dd
finteger <nashassociatesinc@gmail.com>
2024-03-21 11:56:43 -0600

    My Second Commit

--- app.js
+++ app.js
@@ -1,37 +1,42 @@
 const express = require('express');
 const ejs = require('ejs');
+const fs = require('fs').promises;
+require('dotenv').config();
 const app = express();
 
-const port = 3000; 
-
-app.get('/', (req, res) =>{
-
-    const id = req.query.id;
-    const username = req.query.username;
-
-    res.send(`User ID: ${id}.  Username: ${username}`);
-});
 
 
-app.get('/products/:id(\\d+)', (req, res) => {
+app.get('/', async (req, res) =>{
 
-    const productId = req.params.id;
+    try {
 
-    const products = [
-        {"id": 1, "name": "Product A"},
-        {"id": 2, "name": "Product B"},
-        {"id": 3, "name": "Product C"},
-    ];
+     const fileName = "example.txt";
+     const content = "This is the content that is going to be written to file";   
 
-    const product = products.find(product => product.id === parseInt(productId));
+    //Write to the file
+    await fs.writeFile(fileName, content, 'utf-8');
 
-    product ? res.send(`Product id: ${product.id}. Name: ${product.name}`) : 
-    res.status(404).send('Product not found.');
+    const data = await fs.readFile('./example.txt', 'utf-8');
+
+    console.log(data);
+
+    res.send(data);
+
+    } catch (error) {
+        //console.error('Error reading file:', error);
+        //res.status(500).send('Internal Server Error');
+        res.send(error);
+    }
 
 });
 
 
 
+
+
+
+
+const port = process.env.PORT || 3000; 
 
 app.listen(port, () =>{
  console.log(`Listening on port ${port}`);
