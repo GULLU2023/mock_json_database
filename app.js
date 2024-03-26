4403a9147271a7cf2364414390c08de2b7508bda
finteger <nashassociatesinc@gmail.com>
2024-03-26 10:02:02 -0600

    My Commit

--- app.js
+++ app.js
@@ -2,37 +2,16 @@ const express = require('express');
 const ejs = require('ejs');
 const fs = require('fs').promises;
 require('dotenv').config();
+const userRoute = require('./routes/users.js');
 const app = express();
 
 
 
-app.get('/', async (req, res) =>{
+app.use(userRoute);
+app.use(express.static('public'));
 
-    try {
-
-     const fileName = "example.txt";
-     const content = "This is the content that is going to be written to file";   
-
-    //Write to the file
-    await fs.writeFile(fileName, content, 'utf-8');
-
-    const data = await fs.readFile('./example.txt', 'utf-8');
-
-    console.log(data);
-
-    res.send(data);
-
-    } catch (error) {
-        //console.error('Error reading file:', error);
-        //res.status(500).send('Internal Server Error');
-        res.send(error);
-    }
-
-});
-
-
-
-
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 
 
